449369472a356193b808155efa7e75356cf4ddad
Munir Husseini <m.husseini@gmx.de>
2014-04-10 22:03:20 +0200

    Cleaned up code.

--- Sample20130719/Program.cs
+++ Sample20130719/Program.cs
@@ -10,8 +10,8 @@ namespace Sample20130719
         {
             var model = new Model();
             var executer = new Executer();
-            Test3(executer, model, "Numbers");
-            //f(executer, model);
+            var f = Test2(executer, model, "Numbers");
+            f(executer, model);
         }
 
         static Action<Executer, object> Test2(Executer executer, object model, string property)
@@ -41,32 +41,6 @@ namespace Sample20130719
             // f(executer, x)
             var f = (Action<Executer, object>)lambda.Compile();
             return f;
-        }
-        static void Test3(Executer executer, object model, string property)
-        {
-            var modelType = model.GetType();
-            var modelProperty = modelType.GetProperty(property);
-            var calleeType = executer.GetType();
-            var method = calleeType.GetMethod("Do").MakeGenericMethod(modelType, modelProperty.PropertyType.GetGenericArguments().First());
-
-            // executer
-            var executerParameter = Expression.Parameter(calleeType, "executer");
-
-            // x
-            var modelParameter = Expression.Parameter(modelType, "x");
-
-            // y => y.Property
-            var innerParameter = Expression.Parameter(modelType, "y");
-            var lambdaParameter = Expression.Lambda(Expression.Property(innerParameter, modelProperty), innerParameter);
-
-            // Do(x, y => y.Numbers)
-            var call = Expression.Call(executerParameter, method, modelParameter, lambdaParameter);
-
-            // f = (executer, x) => executer.Do(x, y => y.Numbers)
-            var lambda = Expression.Lambda(call, executerParameter, modelParameter);
-
-            // f(executer, x)
-            lambda.Compile().DynamicInvoke(executer, model);
         }
     }
 }
